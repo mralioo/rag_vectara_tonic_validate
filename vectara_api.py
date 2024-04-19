797fd71ddc1c584b20235e3266441911bee34232
alioo <ali.alouane@outlook.de>
2024-04-19 12:19:08 +0200

    ADDED: rerank params for rag tune

--- vectara_api.py
+++ vectara_api.py
@@ -8,26 +8,12 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 from tonic_validate import Benchmark
+from tonic_validate import ValidateApi
 from tonic_validate import ValidateScorer
 from tonic_validate.metrics import AnswerSimilarityMetric
-from tonic_validate import ValidateApi
 
 # loading variables from .env file
 load_dotenv()
-
-TONIC_VALIDATE_API_KEY = os.getenv("TONIC_VALIDATE_API_KEY")
-
-VECTARA_API_KEY = os.getenv("VECTARA_API_KEY")
-CUSTOMER_ID = int(os.getenv("CUSTOMER_ID"))
-CORPUS_ID = 12
-SERVING_ENDPOINT = "api.vectara.io"
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-url = "https://api.vectara.io/v1/query"
-
-
-# url_1 = "http://127.0.0.1:8000/query/?company=Gitlab&prompt=Can%20I%20interview%20for%20multiple%20roles%20at%20the%20same%20time%3F"
 
 
 def _get_rag_best_response(response):
@@ -60,6 +46,9 @@ def get_response(prompt, params):
     num_results = params.get("num_results", 1)
     sentences_before = params.get("sentences_before", 0)
     sentences_after = params.get("sentences_after", 0)
+    lexicalInterpolationConfig = params.get("lexicalInterpolationConfig", {"lambda": 1})
+    diversityBias = params.get("diversityBias", 0.0)
+
 
     payload = {
         "query": [
@@ -81,10 +70,18 @@ def get_response(prompt, params):
                         "customerId": str(CUSTOMER_ID),
                         "corpusId": CORPUS_ID,
                         "semantics": 0,
-                        "lexicalInterpolationConfig": {"lambda": 1},
+                        "lexicalInterpolationConfig": lexicalInterpolationConfig,
                         "dim": [],
                     }
                 ],
+
+                "rerankingConfig": {
+                    "rerankerId": 272725718,
+                    "maxConfig": {
+                        "diversityBias": diversityBias,
+                    }
+                },
+
                 "summary": [
                     {
                         "debug": False,
@@ -145,6 +142,18 @@ def make_scores_df(response_scores):
 
 if __name__ == "__main__":
 
+    # Load the API keys from the environment variables
+    TONIC_VALIDATE_API_KEY = os.getenv("TONIC_VALIDATE_API_KEY")
+
+    VECTARA_API_KEY = os.getenv("VECTARA_API_KEY")
+    CUSTOMER_ID = int(os.getenv("CUSTOMER_ID"))
+    CORPUS_ID = 12
+    SERVING_ENDPOINT = "api.vectara.io"
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    url = "https://api.vectara.io/v1/query"
+
     # specify the version of the tested qa file
     version = "v3"
     qa_file = f"validation_qa_files/qa_gitlab_{version}.json"
@@ -152,10 +161,17 @@ if __name__ == "__main__":
 
     # RAG parameters
     params = {
-        "num_results": 3,
+        "num_results": 10,
         "sentences_before": 5,
-        "sentences_after": 5
+        "sentences_after": 5,
+        "lexicalInterpolationConfig": {"lambda": 2},
+        "diversityBias": 0.5
     }
+
+    # test
+    prompt_test = "how to get hired at Gitlab?"
+
+    response = get_response(prompt_test, params=params)
 
     # Load the QA pairs from the json file for benchmarking
     qa_pairs = []
@@ -183,7 +199,8 @@ if __name__ == "__main__":
         # RetrievalPrecisionMetric(), # This metric is used to compare the precision of the model answers
 
     ]
-    scorer = ValidateScorer(metrics=metrics, model_evaluator="gpt-3.5-turbo")
+    scorer = ValidateScorer(metrics=metrics,
+                            model_evaluator="gpt-3.5-turbo")
 
     response_scores = scorer.score(benchmark,
                                    lambda prompt: get_response(prompt, params=params),
@@ -202,17 +219,19 @@ if __name__ == "__main__":
 
     # Upload the run to the Tonic Validate API
 
-
     validate_api = ValidateApi(TONIC_VALIDATE_API_KEY)
 
     # include the model and version in the metadata and params
 
     run_metadata = {
-        "model": "gpt-3.5-turbo",
-        "version": version,
-    } | params
+                       "model": "gpt-3.5-turbo",
+                       "version": version,
+                   } | params
 
-    validate_api.upload_run(project_id="469df571-c755-4d7b-b74b-b999f5511f1e",
+    # add your project id from the tonic validate project here
+    project_id = "469df571-c755-4d7b-b74b-b999f5511f1e"
+
+    validate_api.upload_run(project_id=project_id,
                             run=response_scores,
                             run_metadata=run_metadata)
 
